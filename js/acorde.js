cf920dcb7fd46a0bee6a1f61d2d3430938830b9b
literame <git config --global user.email>
2024-03-03 21:51:12 -0500

    se añaden los acordes mayores, sin modificacion en html ni logica.

---
+++ js/acorde.js
@@ -0,0 +1,98 @@
+var acordeDoMayor = {
+    nombre: 'Do Mayor',
+    cejilla: false,
+    trastes: {
+        cuerda1: 0, // Cuerda más aguda
+        cuerda2: 1,
+        cuerda3: 0,
+        cuerda4: 2,
+        cuerda5: 3,
+        cuerda6: 'x' // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'Re Mayor',
+    cejilla: false,
+    trastes: {
+        cuerda1: 2, // Cuerda más aguda
+        cuerda2: 3,
+        cuerda3: 2,
+        cuerda4: 0,
+        cuerda5: 0,
+        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'Mi Mayor',
+    cejilla: false,
+    trastes: {
+        cuerda1: 0, // Cuerda más aguda
+        cuerda2: 0,
+        cuerda3: 1,
+        cuerda4: 2,
+        cuerda5: 2,
+        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'Fa Mayor',
+    cejilla: true,
+    trastes: {
+        cuerda1: 1, // Cuerda más aguda
+        cuerda2: 0,
+        cuerda3: 2,
+        cuerda4: 3,
+        cuerda5: 3,
+        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'Sol Mayor',
+    cejilla: false,
+    trastes: {
+        cuerda1: 3, // Cuerda más aguda
+        cuerda2: 0,
+        cuerda3: 0,
+        cuerda4: 0,
+        cuerda5: 2,
+        cuerda6: 3, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'la Mayor',
+    cejilla: false,
+    trastes: {
+        cuerda1: 0, // Cuerda más aguda
+        cuerda2: 1,
+        cuerda3: 1,
+        cuerda4: 1,
+        cuerda5: 0,
+        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+var acordeDoMayor = {
+    nombre: 'Si Mayor',
+    cejilla: true,
+    trastes: {
+        cuerda1: 1, // Cuerda más aguda
+        cuerda2: 3,
+        cuerda3: 3,
+        cuerda4: 3,
+        cuerda5: 0,
+        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
+    }
+};
+
+
+
+
+
+
+
+
