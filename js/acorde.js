aefa67f7750b9962bd202fa5f7e5331e36601233
literame <git config --global user.email>
2024-03-03 21:55:44 -0500

    renombrar variables de acordes

--- js/acorde.js
+++ js/acorde.js
@@ -2,97 +2,89 @@ var acordeDoMayor = {
     nombre: 'Do Mayor',
     cejilla: false,
     trastes: {
-        cuerda1: 0, // Cuerda más aguda
+        cuerda1: 0,
         cuerda2: 1,
         cuerda3: 0,
         cuerda4: 2,
         cuerda5: 3,
-        cuerda6: 'x' // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 'x'
+    }
 };
 
-var acordeDoMayor = {
+var acordeReMayor = {
     nombre: 'Re Mayor',
     cejilla: false,
     trastes: {
-        cuerda1: 2, // Cuerda más aguda
+        cuerda1: 2,
         cuerda2: 3,
         cuerda3: 2,
         cuerda4: 0,
         cuerda5: 0,
-        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 0
+    }
 };
 
-var acordeDoMayor = {
+var acordeMiMayor = {
     nombre: 'Mi Mayor',
     cejilla: false,
     trastes: {
-        cuerda1: 0, // Cuerda más aguda
+        cuerda1: 0,
         cuerda2: 0,
         cuerda3: 1,
         cuerda4: 2,
         cuerda5: 2,
-        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 0
+    }
 };
 
-var acordeDoMayor = {
+var acordeFaMayor = {
     nombre: 'Fa Mayor',
     cejilla: true,
     trastes: {
-        cuerda1: 1, // Cuerda más aguda
+        cuerda1: 1,
         cuerda2: 0,
         cuerda3: 2,
         cuerda4: 3,
         cuerda5: 3,
-        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 0
+    }
 };
 
-var acordeDoMayor = {
+var acordeSolMayor = {
     nombre: 'Sol Mayor',
     cejilla: false,
     trastes: {
-        cuerda1: 3, // Cuerda más aguda
+        cuerda1: 3,
         cuerda2: 0,
         cuerda3: 0,
         cuerda4: 0,
         cuerda5: 2,
-        cuerda6: 3, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 3
+    }
 };
 
-var acordeDoMayor = {
-    nombre: 'la Mayor',
+var acordeLaMayor = {
+    nombre: 'La Mayor',
     cejilla: false,
     trastes: {
-        cuerda1: 0, // Cuerda más aguda
+        cuerda1: 0,
         cuerda2: 1,
         cuerda3: 1,
         cuerda4: 1,
         cuerda5: 0,
-        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 0
+    }
 };
 
-var acordeDoMayor = {
+var acordeSiMayor = {
     nombre: 'Si Mayor',
     cejilla: true,
     trastes: {
-        cuerda1: 1, // Cuerda más aguda
+        cuerda1: 1,
         cuerda2: 3,
         cuerda3: 3,
         cuerda4: 3,
         cuerda5: 0,
-        cuerda6: 0, // Cuerda más grave, 'x' indica que no se debe pulsar
-    }
+        cuerda6: 0
+    }
 };
-
-
-
-
-
-
-
-
