d18496b036f1d8c45f48ad83336206af8975555f
literame <git config --global user.email>
2024-03-08 07:27:14 -0500

    actualizado de funcionalidades

--- js/index.js
+++ js/index.js
@@ -16,6 +16,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const content = document.querySelectorAll('.point');
     const contextMenu = document.getElementById('context-menu');
+    const cejillas = document.querySelectorAll('.cj');
+    const cejillaactiva = document.querySelectorAll('.cejillaactiva');
 
 
     content.forEach(function (element) {
@@ -35,22 +37,51 @@ document.addEventListener('DOMContentLoaded', function () {
         element.addEventListener('click', function (e) {
             if (element.style.backgroundColor === 'white') {
             element.style.backgroundColor = '';
-        } else {
+            } else {
             element.style.backgroundColor = 'white';
-        }
-
-
+            }
         });
-    });
+        });
 
-    document.addEventListener('click', function () {
+        document.addEventListener('click', function () {
         contextMenu.style.display = 'none';
         // element.style.background =  '#e4e4dd62';
     });
 
-
+     cejillas.forEach(function (itmCejilla) {
+        itmCejilla.addEventListener('click', function(c) {
+            if (itmCejilla.style.backgroundColor === 'white') {
+                itmCejilla.style.backgroundColor = '';
+                } else {
+                itmCejilla.style.backgroundColor = 'white';
+            }
+        })
+     });
 });
 
+function mouseSobre() {
+    const cj = document.querySelectorAll('.cj');
+    const vistaAcorde = document.getElementById('vistaAcorde');
+
+    cj.forEach(function (itemCejilla) {
+    vistaAcorde.addEventListener('mouseover', function (event) {
+        if(itemCejilla.style.backgroundColor === 'white'){
+            itemCejilla.style.backgroundColor = 'white';
+        } else{
+            itemCejilla.style.backgroundColor = '#e4e4dd62';
+        }
+
+    });
+    // vistaAcorde.addEventListener('mouseout', function (event) {
+    //     itemCejilla.style.backgroundColor = '';
+        
+    
+    // });
+    });
+}
+
+// Llama a la función cuando se carga el documento
+document.addEventListener('DOMContentLoaded', mouseSobre);
 
 
 
@@ -71,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function convertirYDescargar() {
     // Selecciona la sección que deseas convertir a imagen
-    var seccion = document.getElementById('vista');
+    var seccion = document.getElementById('vistaAcorde');
 
     // Utiliza html2canvas para convertir la sección a una imagen
     html2canvas(seccion).then(function(canvas) {
